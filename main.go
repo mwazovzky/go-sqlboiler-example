b4a6dc7c179fa96d7470cc3a4baefdfee8e2ef44
Mike <mike.wazovzky@gmail.com>
2023-08-26 17:02:46 +0400

    Setup sqlboiler

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
+	"go-sqlboiler/models"
 	"go-sqlboiler/services/config"
 	"go-sqlboiler/services/database"
+	"log"
+
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 func main() {
@@ -10,4 +17,33 @@ func main() {
 
 	db := database.OpenDB(cfg)
 	defer database.CloseDB(db)
+
+	ctx := context.Background()
+
+	count, err := models.Clients().Count(ctx, db)
+	handleErr(err)
+	fmt.Println(count)
+
+	name := fmt.Sprintf("Client #%d", count+1)
+
+	c := &models.Client{Name: name}
+	err = c.Insert(ctx, db, boil.Infer())
+	handleErr(err)
+
+	clients, err := models.Clients().All(ctx, db)
+	handleErr(err)
+	for _, c := range clients {
+		fmt.Printf("%#v\n", *c)
+		fmt.Println()
+	}
+}
+
+func insertClient(db *sql.DB, c *models.Client) error {
+	return c.Insert(context.Background(), db, boil.Infer())
+}
+
+func handleErr(err error) {
+	if err != nil {
+		log.Fatal("error:", err)
+	}
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Go SQL Boiler Example
 
-## Setup
+## Setup database
 
 ```
 export DB_HOST=localhost \
@@ -8,4 +8,24 @@ export DB_HOST=localhost \
     DB_NAME=go_sqlboiler \
     DB_USER=user \
     DB_PASSWORD=usersecret
+```
+
+## Setup sqlboiler
+
+[qlboiler](https://github.com/volatiletech/sqlboiler)
+
+```
+go get github.com/volatiletech/sqlboiler/v4
+go get github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql@latest
+go get github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql@latest
+go get github.com/volatiletech/null/v8
+```
+
+Build sqlboiler and sqlboiler-mysql
+Create sqlboiler.toml with database config
+Generate models
+
+```
+go build
+sqlboiler mysql -c sqlboiler.toml
 ```
